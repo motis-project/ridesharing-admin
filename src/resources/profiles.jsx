b51469eaef537dcfc9b20723ff3ac50fa6187bd4
Philipp <philipp.lerche@sicher3.de>
2023-02-27 23:42:10 +0100

    start

--- src/resources/profiles.jsx
+++ src/resources/profiles.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { 
     List, 
     TextField, 
@@ -13,12 +14,52 @@ import {
     FunctionField,
     Create,
     Show,
-    SimpleShowLayout
+    SimpleShowLayout,
+    Button,
+    TopToolbar,
+    EditButton,
+    Confirm,
+    useShowContext
 } from "react-admin";
+import { supabase } from '../supabase/supabase';
 
 const profileFilters = [
   <TextInput source="q" label="Search" alwaysOn />,
+  
 ];
+const CustomUpdatePostsButton = () => {
+  const {record } = useShowContext();
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => setOpen(true);
+  const handleDialogClose = () => setOpen(false);
+  const handleConfirm = async () => {
+    const {data} = await supabase.rpc('get_my_claims');
+    console.log(data)
+    await supabase.rpc('block_user', { auth_id: record['auth_id'] })
+    setOpen(false);
+  };
+
+  return (
+      <>
+          <Button label="Block User" onClick={handleClick} />
+          <Confirm
+              isOpen={open}
+              title="Block User"
+              content="Are you sure you want to block this User?"
+              onConfirm={handleConfirm}
+              onClose={handleDialogClose}
+          />
+      </>
+  );
+};
+
+const ProfileShowActions = () => (
+  <TopToolbar>
+      <EditButton />
+      <CustomUpdatePostsButton />
+  </TopToolbar>
+);
 
 const gender = [
   { id: 0, name: "male" },
@@ -38,7 +79,7 @@ export const ProfileList = () => (
 );
 
 export const ProfileShow = () => (
-  <Show>
+  <Show actions={<ProfileShowActions />}>
     <SimpleShowLayout>
       <TextField source="id"  />
       <TextField source="auth_id"  />
@@ -54,6 +95,7 @@ export const ProfileShow = () => (
 
 export const ProfileEdit = () => (
   <Edit>
+    <Button> </Button>
     <SimpleForm>
       <TextInput source="id" disabled />
       <TextInput source="auth_id" disabled />
